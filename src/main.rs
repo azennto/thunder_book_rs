8d600aef8a3ecfd2fa1794361255295c8594e9c1
azennto <azenntoyuchi@gmail.com>
2023-05-04 11:45:35 +0900

    座標の構造体

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+const H: usize = 3;
+const W: usize = 4;
+const END_TURN: usize = 4;
+
+#[derive(Debug, Clone)]
+struct Coord {
+    x_: usize,
+    y_: usize,
 }
+impl Coord {
+    fn new() -> Self {
+        Coord { x_: 0, y_: 0 }
+    }
+}
+
+fn main() {}
